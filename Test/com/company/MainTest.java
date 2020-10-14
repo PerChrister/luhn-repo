f37e508a36381ece471ad7f7d8a1be9701ce63da
PerChrister <perchrister96@gmail.com>
2020-10-14 21:24:18 +0200

    Update MainTest.java

--- Test/com/company/MainTest.java
+++ Test/com/company/MainTest.java
@@ -7,25 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     CreditCard creditCard = new CreditCard();
-/*
-    @Test
-    public void checkIfEqual()throws IOException {
-        assertEquals(6,creditCard.validateCreditCard("1234567"));
-        assertEquals(0, creditCard.validateCreditCard("4561237854258962"));
-        assertEquals(2, creditCard.validateCreditCard("4242424242424242"));
-    }
-    @Test
-    public void checkIfNotEqual(){
-        assertNotEquals(6, creditCard.validateCreditCard("12345"));
-        assertNotEquals(2,creditCard.validateCreditCard("3251232584"));
-        assertNotEquals(0,creditCard.validateCreditCard("123456789"));
-    }
-
-    8. if equal -> expected/provided
-    9. if not equal -> expected provided
-    11. if checksum true
-
-*/
 
     @Test
     public void checkIfCreditCardIsTrue(){
